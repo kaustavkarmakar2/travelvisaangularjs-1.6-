9e7bed4d10ba26891a090140be9d570977911c89
Kaustav Karmakar <kaustavkarmakar@Kaustavs-MacBook-Air.local>
2019-08-08 16:35:39 +0530

    first commit

--- app/views/homeScreen/homeScreen.js
+++ app/views/homeScreen/homeScreen.js
@@ -1,4 +1,20 @@
 angular.module('eCommApp')
-    .controller('homeCtrl', ['$rootScope', '$state', function ($rootScope, $state) {      
+    .controller('homeCtrl', ['$rootScope', '$state','$scope', function ($rootScope, $state,$scope) {      
         console.log('homeCtrl--------------calling11111111111111');
+        $scope.tab = 0;
+
+      
+        $scope.previous = function(i){            
+          $scope.tab--;
+       return $scope.tab ;
+      };
+      $scope.setTab = function(newTab){
+        $scope.tab = newTab;
+      };
+  
+      $scope.isSet = function(tabNum){
+        return $scope.tab === tabNum;
+      };
+      $scope.tab++;
+    return $scope.tab ;
 }]);
